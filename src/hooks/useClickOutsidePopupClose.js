023fbe99604ee5e82d2d6476f16e9dbe03cf8eaa
Manoj <manojssankarlingam01@gmail.com>
2024-09-29 18:41:32 +0530

    Build Dashboard

---
+++ src/hooks/useClickOutsidePopupClose.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+const useClickOutsidePopupClose = ( ref, onClose ) => {
+
+  useEffect( () => {
+    const handleClickOutside = ( event ) => {
+      if ( ref.current && !ref.current.contains( event.target ) ) {
+        onClose(); 
+      }
+    };
+
+   
+    document.addEventListener( 'mousedown', handleClickOutside );
+
+    
+    return () => {
+      document.removeEventListener( 'mousedown', handleClickOutside );
+    };
+  }, [ref, onClose] );  
+};
+
+export default useClickOutsidePopupClose;
